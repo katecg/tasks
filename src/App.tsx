b1405746af8ce8fade52c625a7af775bc568f0f8
katecg <katecg@udel.edu>
2024-09-06 18:43:08 -0400

    Revert "Solved html css"

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Button, Container } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -8,52 +7,7 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Kate Geiszler</p>
-            <p>Hello World</p>
-            <h1>Toby the Raccoon</h1>
-            <img src="https://shorturl.at/hSriG" alt="A Picture of a Raccoon" />
-            <p>Tobys favorites things To Do</p>
-            <ul>
-                <li>Sudoku</li>
-                <li>Eating</li>
-                <li>Sleeping</li>
-            </ul>
-            <Button
-                onClick={() => {
-                    console.log("I am logged");
-                }}
-            >
-                Log Hello World
-            </Button>
-            <Container
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    marginTop: "20px",
-                }}
-            >
-                <div
-                    style={{
-                        width: "100px",
-                        height: "200px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "200px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "200px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-            </Container>
+            <p>My name is Kate Geiszler</p>
         </div>
     );
 }
