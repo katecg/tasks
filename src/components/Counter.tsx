a818c0385be190d2fb69d36419d2428a31cb1589
katecg <katecg@udel.edu>
2024-09-25 13:59:58 -0400

    bro

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,9 +3,12 @@ import { Button } from "react-bootstrap";
 
 export function Counter(): React.JSX.Element {
     const [value, setValue] = useState<number>(0);
+    function addOne(): void {
+        setValue(value + 1);
+    }
     return (
         <span>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
+            <Button onClick={addOne}>Add One</Button>
             to {value}.
         </span>
     );
